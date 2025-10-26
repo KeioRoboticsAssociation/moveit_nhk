f12598f0ac196b538259f385a3f775ddd4e4c172
SakataKirato <kiratosakata@keio.jp>
2025-10-26 23:07:23 +0900

    多分もう実験できる

---
+++ build/dynamixel_controller/ament_cmake_python/dynamixel_controller/dynamixel_controller/msg/__init__.py
@@ -0,0 +1,3 @@
+from dynamixel_controller.msg._dynamixel_command import DynamixelCommand  # noqa: F401
+from dynamixel_controller.msg._dynamixel_controller import DynamixelController  # noqa: F401
+from dynamixel_controller.msg._dynamixel_response import DynamixelResponse  # noqa: F401
---
+++ build/dynamixel_controller/ament_cmake_python/dynamixel_controller/dynamixel_controller/msg/_dynamixel_command.py
@@ -0,0 +1,240 @@
+# generated from rosidl_generator_py/resource/_idl.py.em
+# with input from dynamixel_controller:msg/DynamixelCommand.idl
+# generated code does not contain a copyright notice
+
+
+# Import statements for member types
+
+# Member 'ids'
+# Member 'data'
+import array  # noqa: E402, I100
+
+import builtins  # noqa: E402, I100
+
+import rosidl_parser.definition  # noqa: E402, I100
+
+
+class Metaclass_DynamixelCommand(type):
+    """Metaclass of message 'DynamixelCommand'."""
+
+    _CREATE_ROS_MESSAGE = None
+    _CONVERT_FROM_PY = None
+    _CONVERT_TO_PY = None
+    _DESTROY_ROS_MESSAGE = None
+    _TYPE_SUPPORT = None
+
+    __constants = {
+    }
+
+    @classmethod
+    def __import_type_support__(cls):
+        try:
+            from rosidl_generator_py import import_type_support
+            module = import_type_support('dynamixel_controller')
+        except ImportError:
+            import logging
+            import traceback
+            logger = logging.getLogger(
+                'dynamixel_controller.msg.DynamixelCommand')
+            logger.debug(
+                'Failed to import needed modules for type support:\n' +
+                traceback.format_exc())
+        else:
+            cls._CREATE_ROS_MESSAGE = module.create_ros_message_msg__msg__dynamixel_command
+            cls._CONVERT_FROM_PY = module.convert_from_py_msg__msg__dynamixel_command
+            cls._CONVERT_TO_PY = module.convert_to_py_msg__msg__dynamixel_command
+            cls._TYPE_SUPPORT = module.type_support_msg__msg__dynamixel_command
+            cls._DESTROY_ROS_MESSAGE = module.destroy_ros_message_msg__msg__dynamixel_command
+
+    @classmethod
+    def __prepare__(cls, name, bases, **kwargs):
+        # list constant names here so that they appear in the help text of
+        # the message class under "Data and other attributes defined here:"
+        # as well as populate each message instance
+        return {
+        }
+
+
+class DynamixelCommand(metaclass=Metaclass_DynamixelCommand):
+    """Message class 'DynamixelCommand'."""
+
+    __slots__ = [
+        '_command',
+        '_ids',
+        '_address',
+        '_length',
+        '_data',
+    ]
+
+    _fields_and_field_types = {
+        'command': 'uint8',
+        'ids': 'sequence<uint8>',
+        'address': 'uint16',
+        'length': 'uint16',
+        'data': 'sequence<uint8>',
+    }
+
+    SLOT_TYPES = (
+        rosidl_parser.definition.BasicType('uint8'),  # noqa: E501
+        rosidl_parser.definition.UnboundedSequence(rosidl_parser.definition.BasicType('uint8')),  # noqa: E501
+        rosidl_parser.definition.BasicType('uint16'),  # noqa: E501
+        rosidl_parser.definition.BasicType('uint16'),  # noqa: E501
+        rosidl_parser.definition.UnboundedSequence(rosidl_parser.definition.BasicType('uint8')),  # noqa: E501
+    )
+
+    def __init__(self, **kwargs):
+        assert all('_' + key in self.__slots__ for key in kwargs.keys()), \
+            'Invalid arguments passed to constructor: %s' % \
+            ', '.join(sorted(k for k in kwargs.keys() if '_' + k not in self.__slots__))
+        self.command = kwargs.get('command', int())
+        self.ids = array.array('B', kwargs.get('ids', []))
+        self.address = kwargs.get('address', int())
+        self.length = kwargs.get('length', int())
+        self.data = array.array('B', kwargs.get('data', []))
+
+    def __repr__(self):
+        typename = self.__class__.__module__.split('.')
+        typename.pop()
+        typename.append(self.__class__.__name__)
+        args = []
+        for s, t in zip(self.__slots__, self.SLOT_TYPES):
+            field = getattr(self, s)
+            fieldstr = repr(field)
+            # We use Python array type for fields that can be directly stored
+            # in them, and "normal" sequences for everything else.  If it is
+            # a type that we store in an array, strip off the 'array' portion.
+            if (
+                isinstance(t, rosidl_parser.definition.AbstractSequence) and
+                isinstance(t.value_type, rosidl_parser.definition.BasicType) and
+                t.value_type.typename in ['float', 'double', 'int8', 'uint8', 'int16', 'uint16', 'int32', 'uint32', 'int64', 'uint64']
+            ):
+                if len(field) == 0:
+                    fieldstr = '[]'
+                else:
+                    assert fieldstr.startswith('array(')
+                    prefix = "array('X', "
+                    suffix = ')'
+                    fieldstr = fieldstr[len(prefix):-len(suffix)]
+            args.append(s[1:] + '=' + fieldstr)
+        return '%s(%s)' % ('.'.join(typename), ', '.join(args))
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        if self.command != other.command:
+            return False
+        if self.ids != other.ids:
+            return False
+        if self.address != other.address:
+            return False
+        if self.length != other.length:
+            return False
+        if self.data != other.data:
+            return False
+        return True
+
+    @classmethod
+    def get_fields_and_field_types(cls):
+        from copy import copy
+        return copy(cls._fields_and_field_types)
+
+    @builtins.property
+    def command(self):
+        """Message field 'command'."""
+        return self._command
+
+    @command.setter
+    def command(self, value):
+        if __debug__:
+            assert \
+                isinstance(value, int), \
+                "The 'command' field must be of type 'int'"
+            assert value >= 0 and value < 256, \
+                "The 'command' field must be an unsigned integer in [0, 255]"
+        self._command = value
+
+    @builtins.property
+    def ids(self):
+        """Message field 'ids'."""
+        return self._ids
+
+    @ids.setter
+    def ids(self, value):
+        if isinstance(value, array.array):
+            assert value.typecode == 'B', \
+                "The 'ids' array.array() must have the type code of 'B'"
+            self._ids = value
+            return
+        if __debug__:
+            from collections.abc import Sequence
+            from collections.abc import Set
+            from collections import UserList
+            from collections import UserString
+            assert \
+                ((isinstance(value, Sequence) or
+                  isinstance(value, Set) or
+                  isinstance(value, UserList)) and
+                 not isinstance(value, str) and
+                 not isinstance(value, UserString) and
+                 all(isinstance(v, int) for v in value) and
+                 all(val >= 0 and val < 256 for val in value)), \
+                "The 'ids' field must be a set or sequence and each value of type 'int' and each unsigned integer in [0, 255]"
+        self._ids = array.array('B', value)
+
+    @builtins.property
+    def address(self):
+        """Message field 'address'."""
+        return self._address
+
+    @address.setter
+    def address(self, value):
+        if __debug__:
+            assert \
+                isinstance(value, int), \
+                "The 'address' field must be of type 'int'"
+            assert value >= 0 and value < 65536, \
+                "The 'address' field must be an unsigned integer in [0, 65535]"
+        self._address = value
+
+    @builtins.property
+    def length(self):
+        """Message field 'length'."""
+        return self._length
+
+    @length.setter
+    def length(self, value):
+        if __debug__:
+            assert \
+                isinstance(value, int), \
+                "The 'length' field must be of type 'int'"
+            assert value >= 0 and value < 65536, \
+                "The 'length' field must be an unsigned integer in [0, 65535]"
+        self._length = value
+
+    @builtins.property
+    def data(self):
+        """Message field 'data'."""
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        if isinstance(value, array.array):
+            assert value.typecode == 'B', \
+                "The 'data' array.array() must have the type code of 'B'"
+            self._data = value
+            return
+        if __debug__:
+            from collections.abc import Sequence
+            from collections.abc import Set
+            from collections import UserList
+            from collections import UserString
+            assert \
+                ((isinstance(value, Sequence) or
+                  isinstance(value, Set) or
+                  isinstance(value, UserList)) and
+                 not isinstance(value, str) and
+                 not isinstance(value, UserString) and
+                 all(isinstance(v, int) for v in value) and
+                 all(val >= 0 and val < 256 for val in value)), \
+                "The 'data' field must be a set or sequence and each value of type 'int' and each unsigned integer in [0, 255]"
+        self._data = array.array('B', value)
